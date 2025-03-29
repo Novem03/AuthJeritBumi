0d6063bdd60e130dcb6e2fe05fd8de8280123070
MuhammadMiqdad <muhammadmiqdadalfatah@gmail.com>
2025-03-29 22:39:29 +0700

    auth

--- auth/index.js
+++ auth/index.js
@@ -14,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", authRoute);
 
+dotenv.config();
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "Detected" : "Not detected");
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log(
   "EMAIL_PASS:",
